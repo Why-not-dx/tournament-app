7b7be7e9e8baa1407eaab1c0205522c1e5ac8261
anthony Destremx <anthony.destremx@gmail.com>
2023-04-02 23:25:36 +0200

    newtourney page layout

--- tourney_gui.py
+++ tourney_gui.py
@@ -4,7 +4,7 @@ from kivy.lang import Builder
 
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from kivymd.uix.pickers import MDDatePicker
 
 
 class MainPage(Screen):
@@ -37,14 +37,37 @@ class StandingsScreen(Screen):
 class ScreenManager(ScreenManager):
     ...
 
-# runner =  Builder.load_file("tourney.kv")
+
 
 class TourneyApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "DeepPurple"
 
-        # return runner
+    def on_save(self, instance, value, date_range):
+        """
+        event called when the "ok" dialog boc button is clicked
+        :type instance: <kivymd.uix.picker.MDDatePicker object>;
+        :param value: selected date;
+        :type value: <class 'datetime.date'>;
+        :param date_range: list of 'datetime.date' objects in the selected range;
+        :type date_range: <class 'list'>;
+        """
+        print(instance, value, date_range)
+
+
+    def on_cancel(self, instance, value):
+        """
+        Events called when the "CANCEL" dialog box button is clicked.'
+        """
+        print("canceled")
+
+
+    def show_date_picker(self):
+        date_dialog = MDDatePicker(min_year=2022, max_year=2030)
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        date_dialog.open()
+
 
 if __name__ == "__main__":
     TourneyApp().run()
