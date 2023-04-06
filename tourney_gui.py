a7e5c58441805af1b4238eae7c2f0bca4e424ffc
anthony Destremx <anthony.destremx@gmail.com>
2023-04-07 00:50:57 +0200

    fixed function and datepicker bugs

--- tourney_gui.py
+++ tourney_gui.py
@@ -15,15 +15,86 @@ class MainPage(Screen):
 
 
 class PlayersList(Screen):
-    ...
+    def __init__(self, **kwargs):
+        super(PlayersList, self).__init__(**kwargs)
+
+
+    def feed_list(self):
+        # ToDO: replace code with sql query of all players
+        curr_screen = self.parent.get_screen('PlayersList')
+        for x in range(10):
+            curr_screen.ids.players_list.add_widget(
+                OneLineListItem(text=f"Single-line item {x}")
+            )
+
 
 
 class TourneyList(Screen):
-    ...
+    def __init__(self, **kwargs):
+        super(TourneyList, self).__init__(**kwargs)
 
+
+    def on_save(self, instance, value, date_range):
+        """
+        event called when the "ok" dialog boc button is clicked
+        :type instance: <kivymd.uix.picker.MDDatePicker object>;
+        :param value: selected date;
+        :type value: <class 'datetime.date'>;
+        :param date_range: list of 'datetime.date' objects in the selected range;
+        :type date_range: <class 'list'>;
+        """
+
+        curr_screen = self.parent.get_screen('TourneyList')
+        curr_screen.ids.search_tourney_date.text = str(value)
+
+    def on_cancel(self, instance, value):
+        """
+        Events called when the "CANCEL" dialog box button is clicked.'
+        """
+        print("canceled")
+
+    def show_date_picker(self):
+        date_dialog = MDDatePicker(min_year=2022, max_year=2030)
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        date_dialog.open()
+
+
+    def tourney_search(self):
+        #TODO: make the function feed the players list without the error
+        curr_screen = self.parent.get_screen('TourneyList')
+        for x in range(10):
+            curr_screen.ids.players_list.add_widget(
+                OneLineListItem(text=f"Single-line item {x}")
+            )
 
 class NewTourney(Screen):
-    ...
+    def __init__(self, **kwargs):
+        super(NewTourney, self).__init__(**kwargs)
+
+    def on_save(self, instance, value, date_range):
+        """
+        event called when the "ok" dialog boc button is clicked
+        :type instance: <kivymd.uix.picker.MDDatePicker object>;
+        :param value: selected date;
+        :type value: <class 'datetime.date'>;
+        :param date_range: list of 'datetime.date' objects in the selected range;
+        :type date_range: <class 'list'>;
+        """
+
+        curr_screen = self.parent.get_screen('NewTourney')
+        curr_screen.ids.test.text = str(value)
+
+    def on_cancel(self, instance, value):
+        """
+        Events called when the "CANCEL" dialog box button is clicked.'
+        """
+        print("canceled")
+
+    def show_date_picker(self):
+        date_dialog = MDDatePicker(min_year=2022, max_year=2030)
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        date_dialog.open()
+
 
 
 class TournamentScreen(Screen):
@@ -41,8 +112,6 @@ class ScreenManager(ScreenManager):
     ...
 
 
-
-
 class TourneyApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
@@ -56,40 +125,19 @@ class TourneyApp(MDApp):
         Builder.load_file("tourney.kv")
 
     def player_search(self):
-        #TODO: make the function feed the players list without the error
+        #Because of the screen system, calling function returns an empty list
+        #you need to use the root.get_screen('name of screen') to access it like you would access root.
+        curr_screen = self.root.get_screen('PlayersList')
+        #ToDO: replace code with sql query of all players
         for x in range(10):
-            self.root.ids.players_list.add_widget(
+            curr_screen.ids.players_list.add_widget(
                 OneLineListItem(text=f"Single-line item {x}")
             )
+
 
     def light_dark_change(self):
         """change the light or dark mode"""
         self.theme_cls.theme_style = "Light" if self.theme_cls.theme_style == "Dark" else "Dark"
-
-    def on_save(self, instance, value, date_range):
-        """
-        event called when the "ok" dialog boc button is clicked
-        :type instance: <kivymd.uix.picker.MDDatePicker object>;
-        :param value: selected date;
-        :type value: <class 'datetime.date'>;
-        :param date_range: list of 'datetime.date' objects in the selected range;
-        :type date_range: <class 'list'>;
-        """
-        print(instance, value, date_range)
-
-
-    def on_cancel(self, instance, value):
-        """
-        Events called when the "CANCEL" dialog box button is clicked.'
-        """
-        print("canceled")
-
-
-    def show_date_picker(self):
-        date_dialog = MDDatePicker(min_year=2022, max_year=2030)
-        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
-        date_dialog.open()
-
 
 
 if __name__ == "__main__":
